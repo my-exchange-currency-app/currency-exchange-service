fda611fab583d45210a495b5d4ab78f38993458e
ahmed.baz <ahmed.baz@fawry.com>
2023-03-16 23:04:35 +0200

    add more circuit breaker features

--- src/main/java/com/demo/skyros/controller/CircuitBreakerController.java
+++ src/main/java/com/demo/skyros/controller/CircuitBreakerController.java
@@ -1,7 +1,9 @@
 package com.demo.skyros.controller;
 
 import com.demo.skyros.entity.CurrencyExchangeEntity;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +20,14 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "getDefaultCurrencyExchange")
+    //@Retry(name = "sample-api", fallbackMethod = "getDefaultCurrencyExchange")
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "getDefaultCurrencyExchange")
+    @RateLimiter(name = "sample-api") // with 10s , the allowed calls is 10,000(10k)
+    @Bulkhead(name = "sample-api")
     public CurrencyExchangeEntity sampleApi() {
         logger.info("sampleApi for CircuitBreaker");
-        CurrencyExchangeEntity currencyExchangeEntity = testDummyApi();
-        return currencyExchangeEntity;
+        //CurrencyExchangeEntity currencyExchangeEntity = testDummyApi();
+        return new CurrencyExchangeEntity();
     }
 
     private CurrencyExchangeEntity testDummyApi() {
