0cc1dc8971af98cd913551e38f97b0c5d16b6401
ahmed.baz <ahmed.baz@fawry.com>
2023-03-18 14:34:15 +0200

    add zipkin

--- src/main/java/com/demo/skyros/controller/CurrencyExchangeController.java
+++ src/main/java/com/demo/skyros/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.demo.skyros.controller;
 
 import com.demo.skyros.entity.CurrencyExchangeEntity;
 import com.demo.skyros.service.CurrencyExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
-
+    Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
     private CurrencyExchangeService currencyExchangeService;
     @Autowired
@@ -19,6 +21,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchangeEntity exchangeCurrency(@PathVariable String from, @PathVariable String to) {
+        logger.info("currency-exchange from {} to {} ", from, to);
         CurrencyExchangeEntity currencyExchangeEntity = currencyExchangeService.exchangeCurrency(from, to);
         currencyExchangeEntity.setEnvironment(environment.getProperty("local.server.port"));
         return currencyExchangeEntity;
